a1834f05e3de39406944f538aee7017b5bacba79
Guardianff <103484627+Guardianff@users.noreply.github.com>
2025-06-27 19:58:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,7 +118,7 @@ def clean_directory(directory):
 
 # Button layout
 keyboard = InlineKeyboardMarkup([
-    [InlineKeyboardButton("💧𝐖𝐃 𝐙𝐎𝐍𝐄 ™💦", url="https://t.me/am_films")]
+    [InlineKeyboardButton("💧𝙰𝙼_𝙵𝙸𝙻𝙼𝚂💦", url="https://t.me/am_films")]
 ])
 
 # Start command handler
